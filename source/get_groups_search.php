bd716151bca44f76d0d164406d224492fc169589
StepaMalkevich <stepamalkevich@yandex.ru>
2018-05-04 17:41:36 +0300

    add sleep time, hashset for g_id.

--- source/get_groups_search.php
+++ source/get_groups_search.php
@@ -6,8 +6,6 @@
  * Time: 18:06
  */
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
 $access_token = "";
 
 function get_group_members($group_id)
@@ -16,24 +14,39 @@ function get_group_members($group_id)
     $request_params = [
         'group_id' => $group_id,
         'offset' => 0,
-        'count' => 2,
+        'count' => 3,
         'access_token' => $access_token,
         'version' => '5.74'
     ];
 
     $url = 'https://api.vk.com/method/groups.getMembers?' . http_build_query($request_params);
 
-    $result = file_get_contents($url);
+    $repeat = true;
 
-    $result_obj = json_decode($result, true);
+    do {
+        $result = file_get_contents($url);
+        $result_obj = json_decode($result, true);
 
-    $users = $result_obj["response"]["users"];
+        $users = $result_obj["response"]["users"];
 
+        if (!is_null($users)) {
+            $repeat = false;
+        } else {
+            sleep(1);
+        }
+
+    } while ($repeat == true);
+
+    if (is_null($users)) {
+        echo "never";
+        print_r($result);
+    }
     return $users;
 }
 
 $alphas = range('a', 'b');
 $global_matrix = array();
+$hashset = array();
 
 foreach ($alphas as &$value) {
     echo "\n Ключ: $value \n";
@@ -42,7 +55,7 @@ foreach ($alphas as &$value) {
         'q' => $value,
         'type' => 'group',
         'offset' => 0,
-        'count' => 2,
+        'count' => 3,
         'access_token' => $access_token,
         'version' => '5.74'
     ];
@@ -53,23 +66,39 @@ foreach ($alphas as &$value) {
 
     $result_obj = json_decode($result, true);
 
+    $i = 0;
+    $null_cnt = 0;
     foreach ($result_obj['response'] as $group_obj) {
-        $group_id = $group_obj['gid'];
-        if (!is_null($group_id)) {
-            $users = get_group_members($group_id);
-            array_unshift($users, $group_id);
-
-            array_push($global_matrix, $users);
+        if ($i % 100 == 0) {
+            echo "i = $i \n";
         }
+
+        $group_id = $group_obj['gid'];
+
+        if (!array_key_exists($group_id, $hashset)) {
+            $hashset[$group_id] = true;
+
+            if (!is_null($group_id)) {
+                $users = get_group_members($group_id);
+
+                array_unshift($users, $group_id, "<-g_id->users");
+
+                array_push($global_matrix, $users);
+
+                if ($i % 8 == 0) {
+                    sleep(1);
+                }
+            }
+        }
+
+        $i += 1;
     }
 
     unset($value);
 }
 
-$fp = fopen('file.csv', 'w');
-$header = array("group_id, users");
-fputcsv($fp, $header);
 
+$fp = fopen('../data/group_id_users.csv', 'w');
 
 foreach ($global_matrix as $fields) {
     fputcsv($fp, $fields);
